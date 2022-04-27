c9727f23165c2cbb17cee7a4359595e3f54daeb5
MaximusBlue <94325993+MaximusBlue@users.noreply.github.com>
2022-04-27 19:32:47 +1000

    Add files via upload

---
+++ PythagTriadCalculator.py
@@ -0,0 +1,22 @@
+#9's odd index is 4
+
+oddnumber = int(input("Enter odd number: "))
+print("Your odd number is: ")
+print(oddnumber)
+
+oddindex = int(input("Enter odd index: "))
+print("Your odd index is: )")
+print(oddindex)
+
+SecondNumber = (oddnumber) * (oddindex) + (oddindex)
+print("Your second number is: ")
+print(SecondNumber)
+
+ThirdNumber = (SecondNumber) + 1
+print("Your third number is: ")
+print(ThirdNumber)
+
+print("Your completed triad is: ")
+print(oddnumber)
+print(SecondNumber)
+print(ThirdNumber)
